a25d19617b28a719c9cf35b4c4fffc3707a46fec
Islam Nasser <72976111+Islamnasser932@users.noreply.github.com>
2025-11-06 17:45:42 +0200

    Update APP.py

--- APP.py
+++ APP.py
@@ -50,14 +50,14 @@ name_map = {
     "mia.alaxendar": "Mia Alaxendar", "ivy.brooks": "Ivy Brooks", "timothy.williams": "Timothy Williams",
     "sarah.adams": "Sarah Adams", "sara.adams": "Sarah Adams", "samy.youssef": "Samy Youssef",
     "candy.johns": "Candy Johns", "heather.robertson": "Heather Robertson", "a.cabello": "Andrew Cabello",
-    "alia.scott": "Alia Scott", "sandra.sebastian": "Sandra Sebastian", "kayla.miller": "Kayla Miller", "Katty.Crater": "Katty Crater"
+    "alia.scott": "Alia Scott", "sandra.sebastian": "Sandra Sebastian","Katty.Crater": "Katty Crater", "kayla.miller": "Kayla Miller"
 }
 
 samy_chasers = {
     "Emma Wilson", "Scarlett Mitchell", "Lucas Diago", "Mia Alaxendar",
     "Candy Johns", "Sandra Sebastian", "Alia Scott",
-    "Ivy Brooks", "Heather Robertson", "Samy Youssef",
-    "Sarah Adams", "Timothy Williams", "Katty Crater"
+    "Ivy Brooks", "Heather Robertson", "Samy Youssef","Katty Crater",
+    "Sarah Adams", "Timothy Williams"
 }
 
 # ⚠️ Load Raw Data (Initial Check)
@@ -1252,6 +1252,7 @@ elif selected == "Data Analysis":
         
         else:
             st.info("ℹ️ Columns **MCN** and/or **Products** not found in dataset.")
+
 
 
 
